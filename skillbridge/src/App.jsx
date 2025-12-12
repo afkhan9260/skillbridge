8b4971304c81694f3fa2e2f264e5f5179767404b
Fatima Jan <fatimahnseer@gmail.com>
2025-12-12 10:10:16 -0600

    Added Routes to Pages

--- skillbridge/src/App.jsx
+++ skillbridge/src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
+import Tutors from './pages/tutors'
+import Login from './pages/login'
+import About from './pages/about'
+import Contact from './pages/contact'
+import MyProfile from './pages/myprofile'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
+
 
 const App = () => {
   return (
-    <div>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path= '/' element={<Tutors />}/>
+      <Route path= '/tutors/:speciality' element={<Tutors />}/>
+      <Route path= '/login' element={<Login />}/>
+      <Route path= '/about' element={<About />}/>
+      <Route path= '/contact' element={<Contact />}/>
+      <Route path= '/my-profile' element={<MyProfile />}/>
+      <Route path= '/my-appointments' element={<MyAppointments />}/>
+      <Route path= '/appointment/:tutorId' element={<Appointment />}/>
+
+      </Routes>
     
     </div>
   )
---
+++ skillbridge/src/pages/MyAppointments.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyAppointments = () => {
+  return (
+    <div>myappointment</div>
+  )
+}
+
+export default MyAppointments
